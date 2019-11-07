77152e89a499d9315ed5d094d2842cdf5ef287b0
Luigi Muller <sluigimuller@gmail.com>
2019-11-07 16:41:34 -0400

    Checking labels on code

--- montador.py
+++ montador.py
@@ -1,4 +1,43 @@
 codigo = []
+registradores = {}
+opcode_r = {}
+opcode_i = {}
+opcode_j = {}
+
+with open('REGISTERS.txt', 'r') as f: #este arquivo possui os registradores
+    linha = f.readline()
+    while(linha != ''):
+        linha = linha.strip() #retira os espaços no começo e final da linha
+        linha = linha.split() #separa o registrador e o binário
+        registradores[linha[0]] = linha[1] #adiciona no dicionário
+        linha = f.readline()
+
+with open('OPCODE_R.txt', 'r') as f: #este arquivo possui os opcodes do tipo R
+    linha = f.readline()
+    while(linha.strip() != ''):
+        linha = linha.strip() #retira os espaços no começo e final da linha
+        linha = linha.split() #separa o registrador e o binário
+        opcode_r[linha[0]] = linha[1:] #adiciona no dicionário
+
+        linha = f.readline()
+
+with open('OPCODE_J.txt', 'r') as f: #este arquivo possui os opcodes do tipo R
+    linha = f.readline()
+    while(linha.strip() != ''):
+        linha = linha.strip() #retira os espaços no começo e final da linha
+        linha = linha.split() #separa o registrador e o binário
+        opcode_j[linha[0]] = linha[1] #adiciona no dicionário
+
+        linha = f.readline()
+
+with open('OPCODE_I.txt', 'r') as f: #este arquivo possui os opcodes do tipo R
+    linha = f.readline()
+    while(linha.strip() != ''):
+        linha = linha.strip() #retira os espaços no começo e final da linha
+        linha = linha.split() #separa o registrador e o binário
+        opcode_i[linha[0]] = linha[1] #adiciona no dicionário
+
+        linha = f.readline()
 
 with open('teste1.asm', 'r') as f:
     linha = f.readline()
@@ -7,12 +46,24 @@ with open('teste1.asm', 'r') as f:
         linha = linha.partition("#") #separa os comentários
         linha = linha[0] #a linha fica com o código
         linha = linha.strip() #retira os espaços no começo e final da linha
-        print(linha)
-        linha = linha.split(maxsplit=1)
-        print(linha)
-
-        codigo.append(linha)
+        if(linha != ''):#adiciona se não for uma linha vazia
+            codigo.append(linha)
 
         linha = f.readline()
 
 print(codigo)
+
+cont = 0
+while(cont < len(codigo)): #busca por labels
+    if(codigo[cont].count(':') == 1): #busca por labels
+        linha = codigo[cont].partition(':') #separa o código da label
+        print(linha)
+        if(len(linha[0]) ):
+            raise Exception('Sintaxe incorreta')
+        if(!linha[0][0].isalpha()):
+            raise Exception('Nome de label imprópria')
+        for i in linha[0]:
+
+    elif(codigo[cont].count(':') > 1): #não pode haver mais de dois ':'
+        raise Exception('Sintaxe inválida')
+    cont += 1
